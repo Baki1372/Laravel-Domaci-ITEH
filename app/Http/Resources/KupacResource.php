8e82cfdbc3813b7f42fba23ff361c1a990ceeeaf
Baki1372 <nikola-barac@hotmail.com>
2022-04-22 13:58:57 +0200

    Peti zahtev - api rute, kontroleri SlikarController i KupacController, resursi KupacResource i SlikarResource

---
+++ app/Http/Resources/KupacResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class KupacResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'Id: ' => $this->resource->id,
+            'Ime: ' => $this->resource->ime,
+            'Prezime: ' => $this->resource->prezime,
+            'Broj telefona: ' => $this->resource->broj_telefona,
+            'Email: ' => $this->resource->email,
+
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\KupacController;
+use App\Http\Controllers\SlikarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::resource('slikar', SlikarController::class);
+Route::resource('kupac', KupacController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
